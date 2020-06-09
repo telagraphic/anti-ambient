632b2536ddcb81d25e0e4e401992557205675117
telagraphic <telagraphic@gmail.com>
2020-06-09 15:11:29 -0400

    📟 FRONT-END CODE: setup play/stop sound/s and volume control

---
+++ README.md
@@ -0,0 +1,5 @@
+# Range Sliders
+http://danielstern.ca/range.css/?ref=css-tricks#/
+https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/
+https://css-tricks.com/sliding-nightmare-understanding-range-input/
+https://www.impressivewebs.com/onchange-vs-oninput-for-range-sliders/
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,87 @@
-console.log("scripts.js working");
+const soundboardAudio = {
+  ocean: {
+   sound: new Howl({
+     src: ['./sounds/ocean.mp3'],
+     volume: 0,
+     html5: true,
+     loop: true
+   }),
+   details: "sound details"
+ },
+ cafe: {
+   sound: new Howl({
+    src: ['./sounds/cafe.mp3'],
+    volume: 0,
+    html5: true,
+    loop: true
+  }),
+  details: "sound details"
+  },
+  wind: {
+   sound: new Howl({
+     src: ['./sounds/wind.mp3'],
+     volume: 0,
+     html5: true,
+     loop: true
+   }),
+   details: "sound details"
+  }
+}
+
+let select = e => document.querySelector(e);
+let selectAll = e => document.querySelectorAll(e);
+const fadeTimeout = 1000;
+
+function setupSoundButtons() {
+  document.querySelector(".sound-grid__container").addEventListener("click", event => {
+    if (event.target && event.target.matches("section.sound-box")) {
+      //TODO: Header and Artwork are not triggering the click when clicked
+      let buttonSound = event.target.dataset.sound;
+      let currentVolume = event.target.querySelector('.sound-box__volume-slider').dataset.volume;
+
+      if (soundboardAudio[buttonSound].sound.playing()) {
+        soundboardAudio[buttonSound].sound.fade(currentVolume, 0, fadeTimeout);
+
+        setTimeout(function() {
+          soundboardAudio[buttonSound].sound.stop();
+        }, fadeTimeout);
+
+      } else {
+        soundboardAudio[buttonSound].sound.play();
+        soundboardAudio[buttonSound].sound.fade(0, currentVolume, 1000);
+
+        console.log(soundboardAudio[buttonSound].sound.playing());
+        console.log("stop", currentVolume);
+      }
+    }
+  });
+}
+
+function setupVolumeSliders() {
+  const volumeSliders = selectAll('.sound-box__volume-slider');
+
+  volumeSliders.forEach(slider => {
+    slider.addEventListener('click', event => {
+      let sliderSound = event.target.dataset.sound;
+      let currentVolume = event.target.value;
+      soundboardAudio[sliderSound].sound.fade(event.target.dataset.volume, currentVolume, 100);
+      event.target.dataset.volume = event.target.value;
+    })
+  })
+}
+
+function setupStopButton() {
+  const stopButton = select('.stop-button');
+  stopButton.addEventListener('click', function(event) {
+    Howler.stop();
+
+    let soundButtonVolumeSliders = selectAll('.sound-box__volume-slider');
+    soundButtonVolumeSliders.forEach(slider => {
+      slider.dataset.volume = .5;
+    });
+  })
+};
+
+setupStopButton();
+setupVolumeSliders();
+setupSoundButtons();
