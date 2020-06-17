94c68bc96c3001174a595e19d89f25d8c872778a
telagraphic <telagraphic@gmail.com>
2020-06-17 16:19:14 -0400

    👌 IMPROVE: ui tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -119,6 +119,14 @@ function setupSoundButtons() {
   allButtons.forEach(button => {
     button.addEventListener('click', toggleButtonSounds);
   });
+
+  // var hammertime = new Hammer(myElement, myOptions);
+  // if (document.documentElement.clienwidth <= 600) {
+  //   allButtons.forEach(button => {
+  //     button.on('press tap', toggleButtonSounds);
+  //   });
+  // } else {
+  // }
 }
 
 function toggleButtonSounds(event) {
